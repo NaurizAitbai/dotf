00b697c22d4e9557d17c6ad29a1517fd8b32523e
Nauriz Aitbai <nauriz.aitbai@gmail.com>
2020-10-10 16:19:16 +0500

    Massive changes

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,14 +1,90 @@
+# Always restore open sites when qutebrowser is reopened.
 c.auto_save.session = True
-c.content.autoplay = False
-c.content.media_capture = 'ask'
-c.content.notifications = False
-c.content.pdfjs = True
-c.session.lazy_restore = True
-c.tabs.background = True
 
-# Change font to Iosevka
+# Number of commands to save in the command history. 0: no history / -1: unlimited
+c.completion.cmd_history_max_items = 0
+
+# Number of URLs to show in the web history. 0: no history / -1: unlimited
+c.completion.web_history.max_items = 0
+
+# Require a confirmation before quitting the application.
+c.confirm_quit = ['downloads']
+
+# Automatically start playing <video> elements.
+c.content.autoplay = False
+
+# Which cookies to accept.
+c.content.cookies.accept = 'no-3rdparty'
+
+# Allow websites to share screen content.
+c.content.desktop_capture = False
+
+# Limit fullscreen to the browser window (does not expand to fill the screen).
+c.content.fullscreen.window = True
+
+# Allow websites to request geolocations.
+c.content.geolocation = False
+
+# Allow websites to record audio/video.
+c.content.media_capture = False
+
+# Allow websites to lock your mouse pointer.
+c.content.mouse_lock = False
+
+# Allow websites to show notifications.
+c.content.notifications = False
+
+# Allow pdf.js to view PDF files in the browser.
+c.content.pdfjs = True
+
+# Allow websites to request persistent storage quota via navigator.webkitPersistentStorage.requestQuota.
+c.content.persistent_storage = False
+
+# Allow websites to register protocol handlers via navigator.registerProtocolHandler.
+c.content.register_protocol_handler = False
+
+# Which interfaces to expose via WebRTC.
+c.content.webrtc_ip_handling_policy = 'default-public-interface-only'
+
+# What to display in the download filename input.
+c.downloads.location.suggestion = 'both'
+
+# Where to show the downloaded files.
+c.downloads.position = 'bottom'
+
+# Default font familities to use.
 c.fonts.default_family = 'iosevka'
 c.fonts.web.family.standard = 'iosevka'
+
+# Automatically enter insert mode if an editable element is focused after loading the page.
+c.input.insert_mode.auto_load = True
+
+# When to use Chromium's low-end device mode.
+c.qt.low_end_device_mode = 'always'
+
+# Which Chromium process model to use.
+c.qt.process_model = 'process-per-site'
+
+# When/how to show the scrollbar.
+c.scrolling.bar = 'always'
+
+# Enable smooth scrolling for web pages.
+c.scrolling.smooth = True
+
+# When to find text on a page case-insensitively.
+c.search.ignore_case = 'always'
+
+# Load a restored tab as soon as it takes focus.
+c.session.lazy_restore = True
+
+# Open new tabs in the background.
+c.tabs.background = True
+
+# When switching tabs, what input mode is applied.
+c.tabs.mode_on_change = 'restore'
+
+# When to show the tab bar.
+c.tabs.show = 'multiple'
 
 config.set(
         'hints.selectors',
@@ -25,3 +101,5 @@ config.set(
 with config.pattern('*://*.2ch.hk/*') as p:
     p.hints.selectors['all'] = [*p.hints.selectors['all'], '#plashque-close', '.spoiler', '.expand-large-comment']
     p.hints.selectors['scrollable'] = ['.cntnt__left_sticky']
+
+config.load_autoconfig()
--- setup.sh
+++ setup.sh
@@ -1,134 +1,151 @@
 #!/bin/sh
 
+current_location=$(pwd)
+
 # xclip
-expac "%n" -Q xclip >/dev/null || sudo pacman -S --noconfirm xclip
+pacman -Qi xclip >/dev/null 2>&1 || sudo pacman -S --noconfirm xclip
 
-# network-manager-applet
-expac "%n" -Q network-manager-applet >/dev/null || sudo pacman -S --noconfirm network-manager-applet
+# network-manager
+pacman -Qi network-manager-applet >/dev/null 2>&1 || sudo pacman -S --noconfirm network-manager-applet
 
-# pamixer
-expac "%n" -Q pamixer >/dev/null || sudo pacman -S --noconfirm pamixer
-
-# setxkbmap
-expac "%n" -Q xorg-setxkbmap >/dev/null || sudo pacman -S --noconfirm xorg-setxkbmap
+# pulsemixer
+pacman -Qi pulsemixer >/dev/null 2>&1 || sudo pacman -S --noconfirm pulsemixer
 
 # syncthing
-expac "%n" -Q syncthing >/dev/null || sudo pacman -S --noconfirm syncthing
+pacman -Qi syncthing >/dev/null 2>&1 || sudo pacman -S --noconfirm syncthing
 systemctl --user enable syncthing --now
 
 # xsetroot
-expac "%n" -Q xorg-xsetroot >/dev/null || sudo pacman -S --noconfirm xorg-xsetroot
+pacman -Qi xorg-xsetroot >/dev/null 2>&1 || sudo pacman -S --noconfirm xorg-xsetroot
 
 # i3lock
-expac "%n" -Q i3lock >/dev/null || sudo pacman -S --noconfirm i3lock
+pacman -Qi i3lock >/dev/null 2>&1 || sudo pacman -S --noconfirm i3lock
 
 # cmus
-expac "%n" -Q cmus >/dev/null || sudo pacman -S --noconfirm cmus
+pacman -Qi cmus >/dev/null 2>&1 || sudo pacman -S --noconfirm cmus
+
+# nnn
+pacman -Qi nnn >/dev/null 2>&1 || sudo pacman -S --noconfirm nnn
+
+# ntfs-3g
+pacman -Qi ntfs-3g >/dev/null 2>&1 || sudo pacman -S --noconfirm ntfs-3g
 
 # imagemagick
-expac "%n" -Q imagemagick >/dev/null || sudo pacman -S --noconfirm imagemagick
+pacman -Qi imagemagick >/dev/null 2>&1 || sudo pacman -S --noconfirm imagemagick
 
 # noto-fonts
-expac "%n" -Q noto-fonts-cjk >/dev/null || sudo pacman -S --noconfirm noto-fonts-cjk
-expac "%n" -Q noto-fonts-emoji >/dev/null || sudo pacman -S --noconfirm noto-fonts-emoji
-expac "%n" -Q noto-fonts-extra >/dev/null || sudo pacman -S --noconfirm noto-fonts-extra
+pacman -Qi noto-fonts >/dev/null 2>&1 || sudo pacman -S --noconfirm noto-fonts
+pacman -Qi noto-fonts-cjk >/dev/null 2>&1 || sudo pacman -S --noconfirm noto-fonts-cjk
+pacman -Qi noto-fonts-emoji >/dev/null 2>&1 || sudo pacman -S --noconfirm noto-fonts-emoji
+pacman -Qi noto-fonts-extra >/dev/null 2>&1 || sudo pacman -S --noconfirm noto-fonts-extra
 
 # font-config
 [ -d $HOME/.config/fontconfig ] || mkdir -p $HOME/.config/fontconfig
-
-ln -sf $(realpath config/fontconfig/*) $HOME/.config/fontconfig/
+ln -sf $(realpath config/fontconfig/fonts.conf) $HOME/.config/fontconfig/
 
 # mpv
-expac "%n" -Q mpv >/dev/null || sudo pacman -S --noconfirm mpv
-expac "%n" -Q youtube-dl >/dev/null || sudo pacman -S --noconfirm youtube-dl
-
-
-# mpd
-expac "%n" -Q mpd >/dev/null || sudo pacman -S --noconfirm mpd
-
-[ -d $HOME/.config/mpd ] || mkdir -p $HOME/.config/mpd
-
-ln -sf $(realpath config/mpd/*) $HOME/.config/mpd/
-
+pacman -Qi mpv >/dev/null 2>&1 || sudo pacman -S --noconfirm mpv
+pacman -Qi youtube-dl >/dev/null 2>&1 || sudo pacman -S --noconfirm youtube-dl
 
 # transmission
-expac "%n" -Q transmission-cli >/dev/null || sudo pacman -S --noconfirm transmission-cli
-
+pacman -Qi transmission-cli >/dev/null 2>&1 || sudo pacman -S --noconfirm transmission-cli
 [ -d $HOME/.config/transmission-daemon ] || mkdir -p $HOME/.config/transmission-daemon
-
-ln -sf $(realpath config/transmission-daemon/*) $HOME/.config/transmission-daemon/
-
+ln -sf $(realpath config/transmission-daemon/settings.json) $HOME/.config/transmission-daemon/
 
 # feh
-expac "%n" -Q feh >/dev/null || sudo pacman -S --noconfirm feh
+pacman -Qi feh >/dev/null 2>&1 || sudo pacman -S --noconfirm feh
 
 # w3m
-expac "%n" -Q w3m >/dev/null || sudo pacman -S --noconfirm w3m
+pacman -Qi w3m >/dev/null 2>&1 || sudo pacman -S --noconfirm w3m
 
 # poppler
-expac "%n" -Q poppler >/dev/null || sudo pacman -S --noconfirm poppler
+pacman -Qi poppler >/dev/null 2>&1 || sudo pacman -S --noconfirm poppler
 
 # ffmpegthumbnailer
-expac "%n" -Q ffmpegthumbnailer >/dev/null || sudo pacman -S --noconfirm ffmpegthumbnailer
+pacman -Qi ffmpegthumbnailer >/dev/null 2>&1 || sudo pacman -S --noconfirm ffmpegthumbnailer
 
 # ranger
-expac "%n" -Q ranger >/dev/null || sudo pacman -S --noconfirm ranger
-
+pacman -Qi ranger >/dev/null 2>&1 || sudo pacman -S --noconfirm ranger
 [ -d $HOME/.config/ranger ] || mkdir -p $HOME/.config/ranger
-
-ln -sf $(realpath config/ranger/*) $HOME/.config/ranger/
-
+ln -sf $(realpath config/ranger/rc.conf) $HOME/.config/ranger/
+ln -sf $(realpath config/ranger/rifle.conf) $HOME/.config/ranger/
+ln -sf $(realpath config/ranger/scope.sh) $HOME/.config/ranger/
 
 # qutebrowser
-expac "%n" -Q qutebrowser >/dev/null || sudo pacman -S --noconfirm qutebrowser
-
+pacman -Qi qutebrowser >/dev/null 2>&1 || sudo pacman -S --noconfirm qutebrowser
 [ -d $HOME/.config/qutebrowser ] || mkdir -p $HOME/.config/qutebrowser
+ln -sf $(realpath config/qutebrowser/config.py) $HOME/.config/qutebrowser/
 
-ln -sf $(realpath config/qutebrowser/*) $HOME/.config/qutebrowser/
+# pdfjs
+pacman -Qi pdfjs >/dev/null 2>&1 || sudo pacman -S --noconfirm pdfjs
 
+# sxiv
+pacman -Qi sxiv >/dev/null 2>&1 || sudo pacman -S --noconfirm sxiv
+
+# keepassxc
+pacman -Qi keepassxc >/dev/null 2>&1 || sudo pacman -S --noconfirm keepassxc
 
 # dunst
-expac "%n" -Q dunst >/dev/null || sudo pacman -S --noconfirm dunst
-
+pacman -Qi dunst >/dev/null 2>&1 || sudo pacman -S --noconfirm dunst
 [ -d $HOME/.config/dunst ] || mkdir -p $HOME/.config/dunst
-
-ln -sf $(realpath config/dunst/*) $HOME/.config/dunst/
-
+ln -sf $(realpath config/dunst/dunstrc) $HOME/.config/dunst/
 
 # sxhkd
-expac "%n" -Q sxhkd >/dev/null || sudo pacman -S --noconfirm sxhkd
-
+pacman -Qi sxhkd >/dev/null 2>&1 || sudo pacman -S --noconfirm sxhkd
 [ -d $HOME/.config/sxhkd ] || mkdir -p $HOME/.config/sxhkd
-
-ln -sf $(realpath config/sxhkd/*) $HOME/.config/sxhkd/
-
+ln -sf $(realpath config/sxhkd/sxhkdrc) $HOME/.config/sxhkd/
 
 # zathura
-expac "%n" -Q zathura >/dev/null || sudo pacman -S --noconfirm zathura zathura-djvu zathura-pdf-mupdf
-
+pacman -Qi zathura >/dev/null 2>&1 || sudo pacman -S --noconfirm zathura
+pacman -Qi zathura-djvu >/dev/null 2>&1 || sudo pacman -S --noconfirm zathura-djvu
+pacman -Qi zathura-pdf-mupdf >/dev/null 2>&1 || sudo pacman -S --noconfirm zathura-pdf-mupdf
 [ -d $HOME/.config/zathura ] || mkdir -p $HOME/.config/zathura
+ln -sf $(realpath config/zathura/zathurarc) $HOME/.config/zathura/
 
-ln -sf $(realpath config/zathura/*) $HOME/.config/zathura/
+# xdg-user-dirs
+pacman -Qi xdg-user-dirs >/dev/null 2>&1 || sudo pacman -S --noconfirm xdg-user-dirs
+ln -sf $(realpath config/user-dirs.dirs) $HOME/.config/user-dirs.dirs
+ln -sf $(realpath config/user-dirs.locale) $HOME/.config/user-dirs.locale
+[ -d $HOME/downloads ] || mkdir -p $HOME/downloads
+[ -d $HOME/share ] || mkdir -p $HOME/share
+[ -d $HOME/documents ] || mkdir -p $HOME/documents
+[ -d $HOME/music ] || mkdir -p $HOME/music
+[ -d $HOME/pictures ] || mkdir -p $HOME/pictures
+[ -d $HOME/videos ] || mkdir -p $HOME/videos
+[ -d $HOME/containers ] || mkdir -p $HOME/containers
+[ -d $HOME/repositories ] || mkdir -p $HOME/repositories
+[ -d $HOME/qemu ] || mkdir -p $HOME/qemu
+xdg-user-dirs-update
 
+# man
+pacman -Qi man >/dev/null 2>&1 || sudo pacman -S --noconfirm man
+
+# yay
+rm -rf /tmp/yay
+pacman -Qi yay >/dev/null 2>&1 || { git clone https://aur.archlinux.org/yay.git /tmp/yay; cd /tmp/yay && makepkg -si --noconfirm; }
+cd $current_location
+
+# libxft-bgra
+gpg --keyserver pool.sks-keyservers.net --recv-keys 4A193C06D35E7C670FA4EF0BA2FB9E081F2D130E
+yay -Qi libxft-bgra >/dev/null 2>&1 || yes y|yay -S libxft-bgra
+
+# stig
+yay -Qi stig >/dev/null 2>&1 || yay -S --noconfirm stig
+
+# ttf-iosevka
+yay -Qi ttf-iosevka >/dev/null 2>&1 || yay -S --noconfirm ttf-iosevka
 
 # applications (desktop-files)
 [ -d $HOME/.local/share/applications ] || mkdir -p $HOME/.local/share/applications
-
 ln -sf $(realpath applications/*) $HOME/.local/share/applications
-
 
 # share
 [ -d $HOME/.local/share ] || mkdir -p $HOME/.local/share
-
 ln -sf $(realpath share/*) $HOME/.local/share
-
 
 # binaries
 [ -d $HOME/.local/bin ] || mkdir -p $HOME/.local/bin
-
 ln -sf $(realpath bin/*) $HOME/.local/bin
-
-# execution permissions for binaries
 chmod +x bin/*
 
 # link config files
@@ -136,7 +153,5 @@ ln -sf $(realpath config/bashrc) $HOME/.bashrc
 ln -sf $(realpath config/vimrc) $HOME/.vimrc
 ln -sf $(realpath config/xinitrc) $HOME/.xinitrc
 
-
 # link inside .config
-ln -sf $(realpath config/user-dirs.dirs) $HOME/.config/user-dirs.dirs
 ln -sf $(realpath config/mimeapps.list) $HOME/.config/mimeapps.list
